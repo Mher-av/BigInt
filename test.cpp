5a07ed1f4eb2acc710bf0a425f80a123675bf27f
Mher-av <52705935+Mher-av@users.noreply.github.com>
2020-09-11 17:00:41 +0400

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include "BigInt.h"
-using namespace std;
+
 int main()
 {
 	BigInt i("234278477388299839288377266677");
 	BigInt j("475883992884857488277488372738");
 	BigInt k("2");
 	BigInt c = i + j;
-	cout << c.toString()<<endl;
+	std::cout << c.toString()<<endl;
 	c = i - j;
-	cout << c.toString()<<endl;
+	std::cout << c.toString()<<endl;
 	c = i * j;
-	cout << c.toString();
+	std::cout << c.toString();
 	c = j / k;
-	cout << c.toString();
-	system("pause");
+	std::cout << c.toString();
 }
